80692f0bc304a1c4e05ddf31c72fa1b01fc2f182
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-01-24 16:47:30 -0300

    feat: enhance loading indicator with message and update home page to always display it

--- app/_components/loading-indicator.tsx
+++ app/_components/loading-indicator.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const LoadingIndicator: React.FC = () => {
     return (
-        <div className="flex h-full w-full items-center justify-center">
-            <div className="loader h-24 w-24 animate-spin rounded-full border-4 border-t-4 border-dotted"></div>
+        <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+            <div className="loader h-24 w-24 animate-spin rounded-full border-4 border-t-4 border-dotted border-t-primary"></div>
+            <div className="text-xl font-bold text-primary">Carregando...</div>
         </div>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import LoadingIndicator from "./_components/loading-indicator";
 
 const Home = () => {
     const router = useRouter();
-    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         router.push("/pages/home");
-        setIsLoading(true);
     }, [router]);
 
-    return <>{isLoading ? <LoadingIndicator /> : null}</>;
+    return <LoadingIndicator />;
 };
 
 export default Home;
