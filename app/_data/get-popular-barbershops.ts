cb896b45b6af2d7bdab98080c0363f2b37d806f6
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-01-25 21:46:06 -0300

    feat: add function to retrieve and sort popular barbershops by booking count

---
+++ app/_data/get-popular-barbershops.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { db } from "../_lib/prisma";
+
+export const getPopularBarbershops = async () => {
+    const barbershops = await db.barbershop.findMany({
+        orderBy: {
+            name: "desc",
+        },
+        include: {
+            services: {
+                include: {
+                    bookings: true,
+                },
+            },
+        },
+    });
+
+    const barbershopsWithCount = barbershops.map((barbershop) => {
+        const bookingCount = barbershop.services.reduce(
+            (total, service) => total + service.bookings.length,
+            0,
+        );
+        return {
+            ...barbershop,
+            bookingCount,
+        };
+    });
+
+    barbershopsWithCount.sort((a, b) => b.bookingCount - a.bookingCount);
+
+    return barbershopsWithCount;
+};
